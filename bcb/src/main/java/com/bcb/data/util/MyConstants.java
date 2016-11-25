3376ce03c7b6c139d89c3dc8d4770f08e7db3bd4
包青天_2016 <baiqiantao@sina.com>
2016-11-25 16:04:21 +0800

    # Built application files *.apk *.ap_
    # Files for the Dalvik VM
    *.dex
    
    # Java class files
    *.class
    
    # Generated files
    bin/
    gen/
    out/
    
    # Gradle files
    .gradle/
    build/
    
    # Local configuration file (sdk path, etc)
    local.properties
    
    # Proguard folder generated by Eclipse
    proguard/
    
    # Log Files
    *.log
    
    # Android Studio Navigation editor temp files
    .navigation/
    
    # Android Studio captures folder
    captures/
    
    # Intellij
    *.iml

--- bcb/src/main/java/com/bcb/data/util/MyConstants.java
+++ bcb/src/main/java/com/bcb/data/util/MyConstants.java
@@ -5,7 +5,7 @@ public class MyConstants {
 
 	public static final String IDCARDTYPE = "01";
 
-	public static final String HOST_DEV_LOCAL = "http://192.168.1.111:7071";//本地，"http://192.168.1.111:7071"
+	public static final String HOST_DEV_LOCAL = "http://192.168.1.111:5071";//本地，"http://192.168.1.111:7071"
 	public static final String HOST_DEV = "http://ttgateway.100cb.cn";//线下"http://ttgateway.100cb.cn";
 	public static final String HOST_RELEASE = "http://app.fulijr.com";//正式"http://app.fulijr.com";
 
