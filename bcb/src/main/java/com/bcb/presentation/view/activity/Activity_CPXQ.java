3e4a36ba3ffb6231a13efff5b51313f604dfaa4d
包青天_2016 <baiqiantao@sina.com>
2017-03-03 18:57:21 +0800

    2017-3-3

--- bcb/src/main/java/com/bcb/presentation/view/activity/Activity_CPXQ.java
+++ bcb/src/main/java/com/bcb/presentation/view/activity/Activity_CPXQ.java
@@ -126,7 +126,7 @@ public class Activity_CPXQ extends Activity_Base implements View.OnTouchListener
 			buy.setText("售罄");
 			buy.setTextColor(0xff999999);
 			buy.setEnabled(false);
-			buy.setBackgroundColor(0xffe5e5e5);
+			buy.setBackgroundColor(0xffd0d0d0);
 		} else {
 			buy.setText("立即购买");
 			buy.setEnabled(true);
